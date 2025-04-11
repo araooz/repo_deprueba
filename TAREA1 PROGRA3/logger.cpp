6da4fbbe831419ade9262dd7fc6dadd13da5d48e
araooz <araozfali@gmail.com>
2025-04-11 10:15:16 -0500

    que fue lo q no commitee?

--- TAREA1 PROGRA3/logger.cpp
+++ TAREA1 PROGRA3/logger.cpp
@@ -1,1 +1,34 @@
-#include "logger.h"
+#include<fstream>
+#include <string>
+
+using namespace std;
+
+
+namespace file::operations{
+ 
+class logger{
+private:
+    ofstream file;
+public:
+    logger(string name){
+        file.open(name, ios::app);
+    }
+    ~logger(){
+        if(file.is_open()){
+            file.close();}
+    }
+    void write(string m){
+        if(file.is_open()){
+            file << m << endl;}
+    }
+    
+    template<typename T>
+    logger& operator<<(const T& mensaje){
+        if(file.is_open()){
+            file << mensaje << endl;}
+        return *this;
+    }
+
+};
+}
+
